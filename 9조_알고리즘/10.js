662606a9b586e2a2c1f9bd571a7b57bd8008d644
snowflaver <sirayukiyui@gmail.com>
2023-04-08 20:32:06 +0900

    2023-04-08 알고리즘

--- 9조_알고리즘/10.js
+++ 9조_알고리즘/10.js
@@ -11,14 +11,14 @@
 
 function solution(arr1, arr2) {
     let a = []
-    let b = []
     for (let i = 0; i < arr1.length; i++){
+        let b = []
         for (let j = 0; j < arr1[i].length; j++){
-            a.push(arr1[i][j] + arr2[i][j])
+            b.push(arr1[i][j] + arr2[i][j])
         }
-        b.push(a)
+        a.push(b)
     }
-    console.log(b)
+    console.log(a)
 }
 
 console.log(solution([[1,2],[2,3]],[[3,4],[5,6]]))
